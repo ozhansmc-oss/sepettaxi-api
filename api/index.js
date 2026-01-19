19b7f46d53366294438b19fce23b7b357f5f06b0
ozhansmc-oss <ozhansmc@icloud.com>
2026-01-19 17:51:01 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbzE7cKPWbViexMdQYnM4y44YHCAXfNPgLXKS7NCRtkVqyJvJbNhzaTtbP9_y10FvIYn/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwt38xiG663xZjdTgBoOhXPNJYTVqeiJrDF9EnNVpHqELsqouCcOZfULz1M6qn8jrKG/exec";
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
